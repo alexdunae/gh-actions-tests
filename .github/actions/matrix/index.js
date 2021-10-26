8b7da89087461cb22485eb5b6c53b6f9676d61b1
Alex Dunae <alex@dunae.ca>
2021-10-25 18:24:48 -0700

    Actions

--- .github/actions/matrix/index.js
+++ .github/actions/matrix/index.js
@@ -1,26 +1,47 @@
 const core = require("@actions/core");
-// const jmes = require("jmespath");
-const fs = require("fs");
+// const wait = require("./wait");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const inputFilePath = core.getInput("inputFile");
-  const filterString = core.getInput("filter");
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const ms = core.getInput("milliseconds");
+    core.info(`Waiting ${ms} milliseconds ...`);
 
-  core.debug("Inside try block");
-  core.warning("Yolo!");
-  core.debug(`inputFoile ${inputFilePath}`);
-  core.debug(`filter ${filter}`);
+    core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // await wait(parseInt(ms));
+    core.info(new Date().toTimeString());
 
-  //   const inputFile = fs.readFileSync(inputFilePath);
-
-  //   const inputJson = JSON.parse(inputFile);
-
-  //   const filteredIncludes = jmes.search(inputJson, filterString);
-  //   const filteredIncludesAsString = JSON.stringify(filteredIncludes);
-
-  //   const outputString = `{"include":${filteredIncludesAsString}}`;
-  core.setOutput("matrix", "blah");
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput("time", new Date().toTimeString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
+
+// const core = require("@actions/core");
+// // const jmes = require("jmespath");
+// const fs = require("fs");
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const inputFilePath = core.getInput("inputFile");
+//   const filterString = core.getInput("filter");
+
+//   core.debug("Inside try block");
+//   core.warning("Yolo!");
+//   core.debug(`inputFoile ${inputFilePath}`);
+//   core.debug(`filter ${filter}`);
+
+//   //   const inputFile = fs.readFileSync(inputFilePath);
+
+//   //   const inputJson = JSON.parse(inputFile);
+
+//   //   const filteredIncludes = jmes.search(inputJson, filterString);
+//   //   const filteredIncludesAsString = JSON.stringify(filteredIncludes);
+
+//   //   const outputString = `{"include":${filteredIncludesAsString}}`;
+//   core.setOutput("matrix", "blah");
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
