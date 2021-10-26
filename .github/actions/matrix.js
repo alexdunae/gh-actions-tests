4345bb58770fe763a57207391fe15a84735c9612
Alex Dunae <alex@dunae.ca>
2021-10-25 18:16:05 -0700

    Actions

---
+++ .github/actions/matrix.js
@@ -0,0 +1,26 @@
+const core = require("@actions/core");
+// const jmes = require("jmespath");
+const fs = require("fs");
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const inputFilePath = core.getInput("inputFile");
+  const filterString = core.getInput("filter");
+
+  core.debug("Inside try block");
+  core.warning("Yolo!");
+  core.debug(`inputFoile ${inputFilePath}`);
+  core.debug(`filter ${filter}`);
+
+  //   const inputFile = fs.readFileSync(inputFilePath);
+
+  //   const inputJson = JSON.parse(inputFile);
+
+  //   const filteredIncludes = jmes.search(inputJson, filterString);
+  //   const filteredIncludesAsString = JSON.stringify(filteredIncludes);
+
+  //   const outputString = `{"include":${filteredIncludesAsString}}`;
+  core.setOutput("matrix", "blah");
+} catch (error) {
+  core.setFailed(error.message);
+}
